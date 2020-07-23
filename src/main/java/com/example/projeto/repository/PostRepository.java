f542b66528fd8750f16062e199e13b176e5f9c07
Mateus Pires <mateus.dev.ti@gmail.com>
2020-07-23 15:38:25 -0300

    @Query search title

--- src/main/java/com/example/projeto/repository/PostRepository.java
+++ src/main/java/com/example/projeto/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.projeto.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.projeto.entities.Post;
@@ -10,6 +11,9 @@ import com.example.projeto.entities.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
--- src/main/java/com/example/projeto/services/PostService.java
+++ src/main/java/com/example/projeto/services/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
 	
 	
