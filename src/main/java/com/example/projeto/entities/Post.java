fe15dc85ebe6504e3416e8cccfadf5c81192ab06
Mateus Pires <mateus.dev.ti@gmail.com>
2020-07-23 11:16:20 -0300

    Entidade Post

--- src/main/java/com/example/projeto/entities/Post.java
+++ src/main/java/com/example/projeto/entities/Post.java
@@ -1,9 +1,16 @@
 package com.example.projeto.entities;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Post {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "post")
+public class Post implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
 	private String id;
 	private Date date;
 	private String title;
